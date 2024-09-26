badc1a15e96c3e48f9b7b997ff6c19badd68ed2a
unknown <webbdeveloper24@gmail.com>
2024-09-26 18:35:24 +0530

    Empty

--- steelautomation/Invoice_app/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUserPermission(BasePermission):
-    """
-    Custom permission to only allow users with the role of 'Admin' to perform certain actions.
-    """
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'ADMIN'
